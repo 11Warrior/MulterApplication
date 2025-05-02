d8ac5cc919f7331eb5afbf77fd58287f7d57e5ff
Aayush <aayushthapa640@gmail.com>
2025-05-02 13:15:10 +0530

    last proj

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const uploads = multer({dest: 'uploads/'})
 
 var app = express();
 
@@ -11,6 +13,17 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', uploads.single('upfile') ,(req, res) => {
+  console.log(req.file);
+  if (!req.file) {
+    return res.json({error: "No file uploaded"})
+  }
+  res.json({
+    name: req.file.originalname, 
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
